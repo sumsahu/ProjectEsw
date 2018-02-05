a02a659425bd51f09ff13d235f8eb33ba90f20b0
Sumit Sahu <sumsahu@localhost.localdomain>
2018-02-05 09:17:00 +0530

    Introduced main function

--- Embedded_Software/src/Socket.cpp
+++ Embedded_Software/src/Socket.cpp
@@ -4,3 +4,18 @@ Creater : Sumit
 Date :	04/02/2018
 
 */
+
+#include<iostream>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<string.h>
+#include<unistd.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	
+	return 0;
+
+}
